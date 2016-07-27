8926d22b16635dd1f07d91df597b9992eea064e8
Bertware <contact@bertware.net>
2016-07-27 23:52:20 +0200

    Add license header

--- Wlan10/Properties/AssemblyInfo.cs
+++ Wlan10/Properties/AssemblyInfo.cs
@@ -1,12 +1,20 @@
-﻿using System.Reflection;
+﻿// AssemblyInfo.cs in Wlan10/Wlan10
+// Created 2016/07/27
+// ©Bertware, visit http://bertware.net
+// 
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file,
+// you can obtain one at http://mozilla.org/MPL/2.0/.
+
+using System.Reflection;
 using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
 [assembly: AssemblyTitle("Wlan10")]
 [assembly: AssemblyDescription("Wireless network configuration tool for windows 10")]
 [assembly: AssemblyConfiguration("")]
@@ -19,6 +27,7 @@ using System.Windows;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
 //In order to begin building localizable applications, set 
@@ -38,7 +47,7 @@ using System.Windows;
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
     //(used if a resource is not found in the page, 
     // app, or any theme specific resource dictionaries)
-)]
+    )]
 
 
 // Version information for an assembly consists of the following four values:
@@ -51,6 +60,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: NeutralResourcesLanguageAttribute("en")]
+[assembly: NeutralResourcesLanguage("en")]
